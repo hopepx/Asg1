2daa690ed4949a7a36de33b80a590d49c13eb942
hopepx <hopeparadoxsolos@gmail.com>
2022-11-19 16:52:46 +0800

    testing

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const navbar = document.querySelector('.header .navbar');
 const menuButton = document.querySelector('.header .menu');
 
-menuButton.addEventListener('click', () => {
-  navbar.classList.toggle('show');
-  menuButton.classList.toggle('fa-close');
-});
-
 document.onscroll = () => {
-  navbar.classList.remove('show');
-  menuButton.classList.remove('fa-close');
-
   if (window.scrollY > 0) {
     document.querySelector('.header').classList.add('active');
   } else {
